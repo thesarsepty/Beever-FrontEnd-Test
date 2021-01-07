1be3d0338295415ba9cf25053a528bb045e8acbd
hesa <hesamilan@gmail.com>
2021-01-07 11:47:31 +0700

    push

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -6,6 +6,7 @@ import PersonalQuoteView from "./PersonalQuoteView";
 export default function HomeView() {
   return (
     <main style={{ marginLeft: "100px", marginTop: "100px" }}>
+      <a href="https://kanye.rest">API SITE</a>
       <KanyeQuoteView />
       <PersonalQuoteView />
     </main>
