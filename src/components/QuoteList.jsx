e348c14390beb61d25a4a780562798153ad1ef04
hesa <hesamilan@gmail.com>
2021-01-12 15:50:02 +0700

    push

--- src/components/QuoteList.jsx
+++ src/components/QuoteList.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export default function QuoteList({ quotes }) {
   return (
     <ol>
       {quotes.map((quote, idx) => (
-        <li key={idx}>{quote}</li>
+        <li key={idx} style={{ listStyleType: "none" }}>
+          {quote}
+        </li>
       ))}
     </ol>
   );
